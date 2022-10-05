792bcdca682579f7c825de26ee055e5b0830bb09
Rodolfo Romaioli Ribeiro de Jesus <74135777+rodsengcomp@users.noreply.github.com>
2022-10-05 15:17:37 -0300

    Update-05-10-22
    Update-05-10-22

--- sql/sv2xExantnet.sql
+++
@@ -1,5 +0,0 @@
-SELECT exantnet.NU_NOTIFIC, exantnet.DT_NOTIFIC, exantnet.DT_SIN_PRI, exantnet.NM_PACIENT, exantnet.DT_NASC,exantnet.NM_LOGRADO,
-exantnet.NU_NUMERO, exantnet.NM_COMPLEM, exantnet.NM_CONTATO, exantnet.NM_REFEREN, exantnet.CS_VACINA, exantnet.DT_DOSE_N,
-exantnet.CS_VACINAL, exantnet.MENOR_5ANO, exantnet.DE5A14ANOS,exantnet.DE15A39ANO,exantnet.CLASSI_FIN,
-exantnet.CS_DESCART,exantnet.CRITERIO, exantnet.EVOLUCAO, exantnet.DT_OBITO, exantnet.DT_ENCERRA FROM exantnet
-LEFT JOIN sv2 ON exantnet.NU_NOTIFIC = sv2.sinan WHERE exantnet.ID_DISTRIT='70' AND sv2.sinan Is Null
